0ee65e757f77dfd78a56e2b516b8bf42fd6fbef8
victormusembi11 <victor.musembi11@gmail.com>
2022-09-22 16:06:14 +0300

    feat: header title prop

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header>
-        <h1>Task Traker</h1>
+        <h1>{props.title}</h1>
     </header>
   )
 }
