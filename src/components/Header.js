e2fb1687a6b1f0a344511d57089b40179edede1e
victormusembi11 <victor.musembi11@gmail.com>
2022-09-22 16:12:43 +0300

    style: inline styiling for header title

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,19 @@ import React from 'react'
 const Header = ({ title }) => {
   return (
     <header>
-        <h1>{title}</h1>
+        <h1 style={{color: 'green', background: 'black'}}>{title}</h1>
+        <h2 style={headingStyle}>Hello, World</h2>
     </header>
   )
 }
 
 Header.defaultProps = {
-    title: 'Task Traker'
+    title: 'Task Tracker'
+}
+
+const headingStyle = {
+    color: 'white',
+    background: 'black' 
 }
 
 export default Header
